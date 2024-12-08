6066760629e9aff8646669a1035847c2bd55dfdb
carlos <carloscontador1@virginiogomez.cl>
2024-12-08 17:57:34 -0300

    GuiasActivity hecho

---
+++ app/src/main/java/cl/ipvg/happyappv2/Classes/Guia.java
@@ -0,0 +1,40 @@
+package cl.ipvg.happyappv2.Classes;
+
+import static android.content.ContentValues.TAG;
+
+import android.net.DnsResolver;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Guia {
+    private String titulo;
+    private String descripcion;
+
+    public Guia(String titulo, String descripcion) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+
+
+
+
+}
